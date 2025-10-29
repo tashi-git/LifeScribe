5261a1106d261755eb19f6ccfd2253f665384d3a
tashi-git <tashi.github@gmail.com>
2025-10-29 17:21:50 +0530

    update: diary_app | version: latest --> v1

--- db_config.py
+++ db_config.py
@@ -2,7 +2,7 @@ import os
 
 db_config = {
     'host': os.getenv('DB_HOST', 'host.docker.internal'),
-    'user': os.getenv('DB_USER', 'root'),          # your MySQL username
+    'user': os.getenv('DB_USER', 'root'),          
     'password': os.getenv('DB_PASSWORD', 'root@12345'),
     'database': os.getenv('DB_NAME', 'diary_db')
 }
